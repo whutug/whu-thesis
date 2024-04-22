fe9019daf2e3720a0952ff3c433d255943e57d13
SwitWu <2401336502@qq.com>
2024-04-22 21:59:30 +0800

    Add script file to clear auxiliary files

---
+++ scripts/clear.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+rm -f *.aux
+rm -f *.bak
+rm -f *.bbl
+rm -f *.bcf
+rm -f *.blg
+rm -f *.dvi
+rm -f *.fdb_latexmk
+rm -f *.fls
+rm -f *.glg
+rm -f *.glo
+rm -f *.gls
+rm -f *.hd
+rm -f *.idx
+rm -f *.ilg
+rm -f *.ind
+rm -f *.lof
+rm -f *.log
+rm -f *.lot
+rm -f *.out
+rm -f *.run.xml
+rm -f *.sav
+rm -f *.synctex.gz
+rm -f *.synctex\(busy\)
+rm -f *.thm
+rm -f *.toc
+rm -f *.xdv
